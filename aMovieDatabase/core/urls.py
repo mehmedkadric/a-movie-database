6e75939fd698ae5df0ba2624bc698b4696009f74
mehmedkadric <kadricze@gmail.com>
2022-12-30 19:03:27 +0100

    small update

--- aMovieDatabase/core/urls.py
+++ aMovieDatabase/core/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 
 from . import views
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('register/', views.registration, name='register'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='home.html'), name='logout'),
 ]
