876a24c6a878538e092aa8dc6ffa19660abdf3f0
Ahmed Jasarevic <jasarevicahmed999@gmail.com>
2023-01-02 19:16:28 +0100

    Add a toast messages for login/register and also some bug fix.

--- aMovieDatabase/accounts/forms.py
+++ aMovieDatabase/accounts/forms.py
@@ -16,3 +16,21 @@ class NewUserForm(UserCreationForm):
         if commit:
             user.save()
         return user
+
+class RegistrationForm(forms.Form):
+    username = forms.CharField(max_length=30)
+    first_name = forms.CharField(max_length=30)
+    last_name = forms.CharField(max_length=30)
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "Passwords do not match"
+            )
--- aMovieDatabase/accounts/views.py
+++ aMovieDatabase/accounts/views.py
@@ -1,12 +1,8 @@
 from django.contrib import messages
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, get_user_model
 from django.contrib.auth.forms import UserCreationForm
-from .forms import NewUserForm
+from .forms import NewUserForm,RegistrationForm
 
 
 def home(request):
@@ -15,35 +11,32 @@ def home(request):
     }
     return render(request, 'home.html', context)
 
+
 def registration(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
-        email = request.POST['email']
-        password = request.POST['password']
-        confirm_password = request.POST['confirm_password']
-
-        # Validate the form data (omitted for simplicity)
-
-        # Create a new user instance
-        User = get_user_model()
-        new_user = User.objects.create_user(
-            username=username,
-            first_name=first_name,
-            last_name=last_name,
-            email=email,
-            password=password
-        )
-
-        # Save the new user to the database
-        new_user.save()
-
-        # Redirect to a success page
-        return redirect('home')
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            # Create a new user instance
+            User = get_user_model()
+            new_user = User.objects.create_user(
+                username=form.cleaned_data['username'],
+                first_name=form.cleaned_data['first_name'],
+                last_name=form.cleaned_data['last_name'],
+                email=form.cleaned_data['email'],
+                password=form.cleaned_data['password']
+            )
+            # Save the new user to the database
+            new_user.save()
+            # Add a success message
+            messages.success(request, 'Successfully registered!')
+            # Redirect to a success page
+            return redirect('home')
+        else:
+            messages.error(request, 'Error: invalid form')
     else:
-        # Render the form template
-        return render(request, 'register.html')
+        form = RegistrationForm()
+    return render(request, 'register.html', {'form': form})
+
 
 
 def login_view(request):
@@ -53,6 +46,7 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
+            messages.success(request, 'Successfully logged in!')
             return redirect('home')
         else:
             messages.error(request, 'Invalid username or password')  # Store the error message in a cookie
