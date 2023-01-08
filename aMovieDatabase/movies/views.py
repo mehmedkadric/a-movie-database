4f265bc90ae854a571655c296515d625bd1d8f52
kosharun <kosharun04@gmail.com>
2023-01-09 00:27:17 +0100

    homepage and movies edit, top 3 movies feature

--- aMovieDatabase/movies/views.py
+++ aMovieDatabase/movies/views.py
@@ -7,7 +7,19 @@ import json
 def movie(request):
     movies = Movie.objects.all()
     review_images =ReviewImage.objects.values('caption', 'image').distinct()
-    context = {'movies': movies, 'review_images': review_images}
+
+    matching_movies = []
+
+    for review_image in review_images:
+        caption = review_image['caption']
+        for movie in movies:
+            if movie.title == caption:
+                matching_movies.append(movie)
+
+    # Sort the movies by vote_average in descending order
+    sorted_movies = sorted(matching_movies, key=lambda x: x.vote_average, reverse=True)[:3]
+
+    context = {'movies': movies, 'review_images': review_images,  'sorted_movies': sorted_movies}
     return render(request, 'movies.html', context)
  
  
@@ -21,3 +33,4 @@ def movie_detail(request, title):
     movie['genres'] = [d['name'] for d in x]
     # Render the reviews template with the movie reviews
     return render(request, 'movie_info.html', {'movie': movie, 'reviews': reviews , 'review_image':  review_image})
+
