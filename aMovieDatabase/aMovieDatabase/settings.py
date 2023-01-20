4897368ccf4de6fe657a51f95c378a9a6bf3d285
kosharun <kosharun04@gmail.com>
2023-01-20 20:26:17 +0100

    add a contact page

--- aMovieDatabase/aMovieDatabase/settings.py
+++ aMovieDatabase/aMovieDatabase/settings.py
@@ -30,6 +30,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 # Application definition
 
--- aMovieDatabase/core/urls.py
+++ aMovieDatabase/core/urls.py
@@ -7,5 +7,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('contact/', views.contact_view, name='contact_view'),
 ]
 
