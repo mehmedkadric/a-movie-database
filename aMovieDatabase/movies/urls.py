0cbf9ead0ec6695bd4c04468bd3735a367876caf
Ahmed Jasarevic <jasarevicahmed999@gmail.com>
2023-01-22 13:15:05 +0100

    Add a movie feature

--- aMovieDatabase/movies/urls.py
+++ aMovieDatabase/movies/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth import views as auth_views
  
 urlpatterns = [
     path('movies/', views.movie, name='movie'),
+    path('add_movie/', views.add_movie, name='add_movie'),
     path('watchlist/', views.watchlist, name='watchlist'),
     path('movies/details/<str:title>/', views.movie_detail, name='movie_detail'),
 ]
