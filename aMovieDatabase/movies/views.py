4bc18beaf4e84c86f13c6e7903a6acff18f28e44
Ahmed Jasarevic <jasarevicahmed999@gmail.com>
2023-01-22 13:22:43 +0100

    Add a keyword feature

--- aMovieDatabase/movies/views.py
+++ aMovieDatabase/movies/views.py
@@ -56,6 +56,8 @@ def movie_detail(request, title):
     movie_image = MovieImage.objects.filter(caption=title).first()
     x = json.loads(movie['genres'])
     movie['genres'] = [d['name'] for d in x]
+    y = json.loads(movie['keywords'])
+    movie['keywords'] = [d['name'] for d in y]
     hours = movie['runtime'] // 60
     minutes = movie['runtime'] % 60
     formatted_runtime = f"{hours} hours {minutes} minutes"
